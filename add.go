73ef5fd48431f0e02999ac7734ca467c83bb5695
zfirdavs <firdavs792@gmail.com>
2024-02-21 15:52:41 +0500

    feat: add godoc comments

--- add.go
+++ add.go
@@ -1,5 +1,8 @@
+// Package number implements the operations within numbers
 package number
 
+// Add is a function that takes in two integer values and returns their sum.
+// It follows the rules specified by https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
